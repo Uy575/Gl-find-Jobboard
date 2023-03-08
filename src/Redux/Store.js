bb47e6588fbb54270f5e8c89ae4509f5c9307886
Uy575 <usamauy57@gmail.com>
2023-03-08 15:16:18 +0500

    redux location and autocomp reduer added

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -3,12 +3,14 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import topJobReduer from "./TopJobReduer";
 import JobReducer from './JobReducer'
+import LocationAndJobTypeReducer from "./LocationAndJobTypeReducer"
 
 const store  = configureStore ({
 
     reducer: {
         topProducts : topJobReduer,
         JobReducer : JobReducer,
+        LocationAndJobTypeReducer : LocationAndJobTypeReducer,
     },
 },composeWithDevTools());
 
