b9a5c92087fd0dd260ce7ac4e2c048610cf132c9
Uy575 <usamauy57@gmail.com>
2023-03-20 16:30:47 +0500

    card and buttons border rework

--- src/Components/Detail-Page-Components/Get-Sia-License/GetSiaLicense.jsx
+++ src/Components/Detail-Page-Components/Get-Sia-License/GetSiaLicense.jsx
@@ -12,7 +12,7 @@ function GetSiaLicense() {
       <div className='getLicensedContent'>
             <h4 style={{color:'rgb(59,213,90)',fontWeight:'bolder'}}>How to get an SIA Licence</h4>
             <p>It is a legal requirement to get an SIA licence to work in the private security industry. Learn how you can apply for your licence and work legally as a Door Supervisor, CCTV Operator, Security Guard or Close Protection Officer.</p>
-            <Link to='https://www.get-licensed.co.uk/licence/get-sia-licence'>
+            <Link to='https://www.get-licensed.co.uk/licence/get-sia-licence' target='_blank'>
             <button className='getLicensedButton'>Learn more...</button>
             </Link>
       </div>
