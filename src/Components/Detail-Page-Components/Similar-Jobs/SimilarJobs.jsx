b3f383f963b6a7b8743a73681a0db75f3a69b4ef
Uy575 <usamauy57@gmail.com>
2023-03-20 15:50:10 +0500

    link added to similar jobs

--- src/Components/Detail-Page-Components/Similar-Jobs/SimilarJobs.jsx
+++ src/Components/Detail-Page-Components/Similar-Jobs/SimilarJobs.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import SimilarJobList from "./Similar-Job-List/SimilarJobList";
 import "./similarjob.css";
+import { Link } from "react-router-dom";
 const SimilarJobs = ({similarJob,id}) => {
   return (
     <section >
       <div className="sj-text">
         <h4>Similar Jobs</h4>
-        <a href="">View all</a>
+        <Link to='/jobs?title=&city=&venue=&sia-licence=&distance=30&salary-min=9&salary-max=50&lat=&lng`'> View all </Link>
       </div>
       <SimilarJobList similarJob={similarJob} id={id}  />
       
